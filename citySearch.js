04d37f1001c735512a78e3d4fbbf3fa911792616
rose-gd-artist <61298013+rose-gd-artist@users.noreply.github.com>
2020-10-05 23:16:15 -0400

    filter function fixed and added commafy fx

--- citySearch.js
+++ citySearch.js
@@ -297,6 +297,58 @@
 
 // jobNames(employees) // teachers solution
 
+/*
+Create a search bar that filters a list of cities by name.
+
+https://www.javascripttutorial.net/javascript-array-filter/
+
+1. Display a page with the cities below, their population,
+and a famous landmark. Use javascript to populate the page.
+
+2. Create a search bar using the input tag. When a user types
+into the search bar only the cities where the search characters
+overlap should be displayed on the page. Make the search bar
+case-insensitive. For example: If a user types in 'hou' the
+page should only show Houston.
+
+Grading criteria:
+
+1. Use the filter() function to complete this assignment.
+
+2. There should be at least two functions. One should populate
+the page when the page loads. The other should filter when
+the user types into the search bar.
+
+3. Use only ES6+ techniques if needed: template strings (${}),
+arrow functions, forEach(), etc.
+
+4. Use GitHub and make at least 5 commits with meaningful commit
+messages in the present tense. For example: "Create filter function."
+
+5. Do not have any extraneous comments beyond explanations of
+code (if needed) in your final result. Do not have any console.logs,
+in comments or otherwise.
+
+6. CSS is not necessary and will not be graded but is encouraged 
+for the sake of practice.
+
+Ungraded Stretch Goals:
+
+1. Create a dropdown using the select element. The dropdown 
+will have two options: name and landmark. The selected option 
+will change how the list is being filtered. For instance, if 
+the landmark option is chosen the search bar will filter using 
+the text in landmark instead of name.
+
+2. Use the sort() function to sort the cities by population 
+on load. Do not manually shift the objects within the array.
+
+3. Create a button that, when clicked, changes the list from 
+sorted ascending by population to sorted descending by 
+population, and vice-versa.
+
+*/
+
 
 const cities = [
     { name: "Houston", population: 2099451, landmark: "NASA Space Center" },
@@ -315,30 +367,10 @@ function showGreeting(){
 }
 showGreeting()
 
-// function getSearchResult(event){
-
-//     let searchTerm = event.target.value;
-//     let sVal = searchTerm.toLowerCase();
-
-//     const filteredCities = cities.filter((city) => {
-//         return sVal === city.name;
-//     })
-    
-//     const cityHTML = filteredCities.map((city) => {
-//         return `<div class="cityName">${city.name}</div>`
-//     })
-//     result.innerHTML += cityHTML;
-
-//     return result;
-
-// }
-
-//searchBar.addEventListener("keyup", getSearchResult);
-
 const displayCities = (bigTown) => {
     const cityListings = bigTown.map(bigTown => {
         return `<div class="cityName">City: ${bigTown.name}</div>
-                <div class="cityPop">Population: ${bigTown.population}</div>
+                <div class="cityPop">Population: ${commafy(bigTown.population)}</div>
                 <div class="cityLandmark">Landmark: ${bigTown.landmark}</div>`;
     });
     result.innerHTML = cityListings;
@@ -347,26 +379,35 @@ const displayCities = (bigTown) => {
 displayCities(cities)
 
 function cityFiltered(event) {
+    searchBar = document.getElementsByClassName("searchBar")[0];
     let inSearchBar = event.target.value;
     let sVal = inSearchBar.toLowerCase();
+    let answer = searchBar.value.toLowerCase();
 
-    const selectedCity = cities.filter((city) => {
-        if(inSearchBar.value === city.name){
-            return inSearchBar === city.name.toLowerCase().includes(sVal);
-        }
+    let selectedCity = cities.filter(city => city.name.toLowerCase().includes(sVal));
 
-    });
-    
-    const searchEntered = selectedCity.map((city) => {
+    result.innerHTML = "";
 
-            return `<div class="cityName">City: ${city.name}</div>
-                <div class="cityPop">Population: ${city.population}</div>
-                <div class="cityLandmark">Landmark: ${city.landmark}</div>`;
-            
+    if(answer === sVal){
+        let searchEntered = selectedCity.map((selectedCity) => {
 
-    });
-    result.innerHTML += searchEntered.join("");
-    //return result;
+            return `<div class="cityName">City: ${selectedCity.name}</div>
+                <div class="cityPop">Population: ${commafy(selectedCity.population)}</div>
+                <div class="cityLandmark">Landmark: ${selectedCity.landmark}</div>`;
+        });
+        result.innerHTML += searchEntered;
+    }
+};
+
+function commafy(num) {
+    var str = num.toString().split('.');
+    if (str[0].length >= 5) {
+        str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+    if (str[1] && str[1].length >= 5) {
+        str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+    }
+    return str.join('.');
 };
 
 searchBar.addEventListener('keyup', cityFiltered)
